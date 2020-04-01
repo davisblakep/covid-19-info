66e26cab402a041486f5b6ed34d9ef4bf5892900
Blake Davis <blakedavis@Blakes-MBP.lan1>
2020-03-31 21:28:53 -0400

    Changed basic styling colors

--- covid-19-info/src/Components/Navbar.js
+++ covid-19-info/src/Components/Navbar.js
@@ -7,6 +7,8 @@ import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 
+
+
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
@@ -24,13 +26,13 @@ export default function Navbar() {
 
   return (
     <div className={classes.root}>
-      <AppBar position="static">
+      <AppBar style={{ background: '#37474f' }} position="static">
         <Toolbar>
           <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
             <MenuIcon />
           </IconButton>
           <Typography variant="h6" className={classes.title}>
-            COVID-19 Latest Information
+            COVID-19 Status Updates
           </Typography>
           <Button color="inherit">Login</Button>
         </Toolbar>
