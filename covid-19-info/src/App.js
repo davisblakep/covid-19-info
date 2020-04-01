1bd32508eb88ba5af182105614ab8921a18430d8
Blake Davis <blakedavis@Blakes-MBP.lan1>
2020-03-31 20:36:47 -0400

    Installed initial dependencies, added Navbar, added basic landing page with photo from pexels

---
+++ covid-19-info/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+import Navbar from './Components/Navbar'
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <div className='main-background-img'></div>
+      <header className="App-header">
+      </header>
+    </div>
+  );
+}
+
+export default App;
